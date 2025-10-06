4404a6dae929c62e3d08d4e8050a85e68d0ca3c6
redaitoronto <redaitoronto@gmail.com>
2025-10-06 17:09:57 +0300

    Fix webpack config for Privy

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding')
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    }
+    return config
+  },
 }
 
 export default nextConfig
